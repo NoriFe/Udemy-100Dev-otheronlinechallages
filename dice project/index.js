0fb5c6b3f451d68d999632686d56fdccd403a782
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2023-11-28 10:56:17 +0000

    Update index.js

--- dice project/index.js
+++ dice project/index.js
@@ -1,8 +1,8 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".svg"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".svg"; 
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage; 
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -16,7 +16,7 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".svg";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
-//If player 1 wins
+
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Play 1 Wins!";
 }
